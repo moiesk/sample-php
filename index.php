512d971a2835a20f863ef2f6e48510ea08222bab
Moises Eskinazi <moiesk@gmail.com>
2021-02-02 12:38:50 -0500

    Adding monolog to app

--- index.php
+++ index.php
@@ -2,12 +2,26 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Cowsayphp\Farm;
+use Monolog\Logger;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
 
 header('Content-Type: text/plain');
 
+$output = '[%datetime%] %channel%.%level_name%: %message% %context% %extra%';
+$formatter = new LineFormatter($output);
+
+$log = new Logger("CowApp");
+$streamhandler = new StreamHandler('php://stdout', Logger::WARNING);
+$streamhandler->setFormatter($formatter);
+$log->pushHandler($streamhandler);
+
 $text = "Set a message by adding ?message=<message here> to the URL";
 if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = $_GET['message'];
+  $log->warning('User sent message');
+  $text = $_GET['message'];
+} else {
+  $log->warning('No message provided');
 }
 
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
